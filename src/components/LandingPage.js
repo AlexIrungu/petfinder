cb3456e90af5a3665f9de0917c744fda516eb290
AlexIrungu <aleaxmuiruri@gmail.com>
2023-03-02 11:51:39 +0300

    created the landing page

---
+++ src/components/LandingPage.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function LandingPage(){
+    return(
+        <div>
+            <nav className="navbar">
+                <div className="container-fluid">
+                    <a className="navbar-brand" id="home-link" href="">Home</a>
+                    <a href="signup"><button className="btn">SignUp</button></a>
+                    <a href="login"><button className="btn">LogIn</button></a>
+
+                </div>
+                
+
+            </nav>
+        </div>
+    )
+
+}
+export function LandingPage()
